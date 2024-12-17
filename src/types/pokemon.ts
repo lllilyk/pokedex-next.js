3374562571b7581a8354f2c8de3213332beddf73
lllilyk <ym0402kkk@naver.com>
2024-12-17 17:32:44 +0900

    포켓몬 PokemonDetail 인터페이스 추가

--- src/types/pokemon.ts
+++ src/types/pokemon.ts
@@ -16,3 +16,25 @@ export interface BookmarkPokemon {
         front_default: string;
     };
 }
+
+export interface PokemonDetail {
+    pokemonId: number;
+    name: string;
+    koreanName: string;
+    genus: string;
+    sprites: {
+        front_default: string;
+    };
+    types: {
+        name: string;
+        _id: string;
+    }[];
+    stats: {
+        stat: {
+            name: string;
+        };
+        base_stat: string;
+        _id: string;
+    }[];
+    viewCount: number;
+}
